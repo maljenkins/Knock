f8fa5a29ee675a87b10f5b4c67bff2d0ceadef92
maljenkins <53225525+maljenkins@users.noreply.github.com>
2020-07-09 06:21:51 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require("gulp");
+const nunjucksRender = require("gulp-nunjucks-render");
+const sass = require("gulp-sass");
+const data = require("gulp-data");
+
+gulp.task("default", function () {
+  return gulp
+    .src("src/templates/*.html")
+    .pipe(
+      data(function () {
+        return require("./src/data/global.json");
+      })
+    )
+    .pipe(
+      nunjucksRender({
+        path: ["src/templates/"], // String or Array
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+// Compile scss
+
+function style() {
+  // Locate scss file
+  return (
+    gulp
+      .src("src/scss/**/*.scss")
+      // Pass through scss compiler
+      .pipe(sass())
+      // Destination for compiled css
+      .pipe(gulp.dest("dist/css/"))
+  );
+}
+exports.style = style;
+
+
+gulp.task("watch", function () {
+  gulp.watch('src/**/*', gulp.series(['default']));
+});
